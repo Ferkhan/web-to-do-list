500c8968aee126ba2fb5b894f0412198ee603566
Ferkhan <133190285+Ferkhan@users.noreply.github.com>
2025-04-10 19:40:42 -0500

    update project

--- app.js
+++ app.js
@@ -2,64 +2,76 @@
 const form = document.querySelector("#formulario_input");
 const btnAgregar = document.querySelector("#btn_agregar");
 const listaU = document.querySelector("#lista_todo");
+
 let tareas = [];
 
+// 2. LISTENERS
 btnAgregar.addEventListener("click", function(event) {
     event.preventDefault();
+    
+    // EXTRAER EL TEXTO QUE INGRESÓ EL USUARIO
     const txtInput = form.txt_input.value;
     agregarTarea(txtInput);
 })
 
 
+// 3. FUNCIONES
 function agregarTarea(txtInput) {
     if (txtInput.length > 0) {
         tareas.push(txtInput);
-        // crearTarea(txtInput);
         actualizarTareas();
-        form.txt_input.value = "";
+        form.txt_input.value = ""
     }
+        
 }
 
 function crearTarea(txtInput, index) {
+    // CREAR UN ELEMENTO DE LISTA  
     const item = document.createElement("li");
-    // item.innerText = txtInput;
-    
+
+    // CREAR UN ID IDEPENDIENTE
     const itemID = "tarea_" + index;
-    item.setAttribute("class", "item_todo");
+
+    // CONTENIDO DE ELEMENTO DE LISTA
     item.innerHTML = `
-        <input type="checkbox" id="${itemID}" class="check">
-        <label for="${itemID}" class="check_marcador">
-            <i class="bi bi-circle"></i>
-        </label>
-        <label for="${itemID}" class="txt_todo">
-            ${txtInput}
-        </label>
-        <button class="btn_eliminar"><i class="bi bi-trash"></i></button>
+        <li class="item_todo">
+            <input type="checkbox" id="${itemID}" class="check">
+            <label for="${itemID}" class="check_marcador">
+                <i class="bi bi-circle"></i>
+            </label>
+            <label for="${itemID}" class="txt_todo">
+                ${txtInput}
+            </label>
+            <button class="btn_eliminar">
+                <i class="bi bi-trash"></i>
+            </button>
+        </li>
     `
+
+    const checkbox = item.querySelector(".check")
+    checkbox.addEventListener("change", () => {
+        // ENCONTRAR EL i DENTRO DE LABEL
+        const icono = checkbox
+        .closest("li")
+        .querySelector(".check_marcador i");
+
+        // CAMBIAR CONTENIDO DE ICONO CON RESPECTO A SI ESTÁ MARCADO O NO
+        if (checkbox.checked) {
+            icono.classList.remove("bi-circle");
+            icono.classList.add("bi-check-circle-fill");
+        } else {
+            icono.classList.remove("bi-check-circle-fill");
+            icono.classList.add("bi-circle");
+        }
+    });
 
     const btnEliminar = item.querySelector(".btn_eliminar");
     btnEliminar.addEventListener("click", () => {
         eliminarTarea(index);
     })
 
-    const checkbox = item.querySelector(".check");
-    checkbox.addEventListener('change', () => {
-        // Encuentra el <i> dentro del label .check_marcador asociado al checkbox
-        const icono = checkbox
-            .closest('li')
-            .querySelector('.check_marcador i');
-
-        // Cambiar el icono dependiendo si está marcado o no
-        if (checkbox.checked) {
-            icono.classList.remove('bi-circle');
-            icono.classList.add('bi-check-circle-fill');
-        } else {
-            icono.classList.remove('bi-check-circle-fill');
-            icono.classList.add('bi-circle');
-        }
-    });
-    // listaU.append(item);
     return item;
+    
 }
 
 function actualizarTareas() {
@@ -71,6 +83,6 @@ function actualizarTareas() {
 }
 
 function eliminarTarea(index) {
-    tareas = tareas.filter((_, i) => i !== index)
+    tareas = tareas.filter((_, i) => i !== index);
     actualizarTareas();
 }
